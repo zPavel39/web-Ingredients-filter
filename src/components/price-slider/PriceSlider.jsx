673a6836482ac8e5a70c2d0bd86df9101de0f740
zPavel39 <golubev33108ai@gmail.com>
2023-08-16 11:02:33 +0200

    init: input value range

--- src/components/price-slider/PriceSlider.jsx
+++ src/components/price-slider/PriceSlider.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import './PriceSlider.scss'
@@ -13,17 +13,24 @@ const PriceSlider = ({ ...props }) => {
   }
 
   return (
-    <Box sx={{ width: 300 }}>
-      <Slider
-        getAriaLabel={() => 'Temperature range'}
-        value={props.priceRangeValue}
-        min={props.minPrice}
-        max={props.maxPrice}
-        step={10}
-        onChange={callback.setPriceRangeValue}
-        valueLabelDisplay="auto"
-      />
-    </Box>
+    <div className='priceSlider'>
+      <div className='priceSlider__head'>
+        <input className='priceSlider__head_input' type='number' value={props.priceRangeValue[0]} onChange={(e) => props.setPriceRangeValue([e.target.value, props.priceRangeValue[1]])} />
+        <input className='priceSlider__head_input' type='number' value={props.priceRangeValue[1]} onChange={(e) => props.setPriceRangeValue([props.priceRangeValue[0], e.target.value])} />
+      </div>
+      <Box sx={{ width: 300 }}>
+        <Slider
+          getAriaLabel={() => 'Temperature range'}
+          value={props.priceRangeValue}
+          min={props.minPrice}
+          max={props.maxPrice}
+          step={10}
+          onChange={callback.setPriceRangeValue}
+          valueLabelDisplay="auto"
+          color="secondary"
+        />
+      </Box>
+    </div>
   );
 }
 export default PriceSlider
